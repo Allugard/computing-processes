d3e2f6133b9e35e1aff359916087fce14567d89b
Allugard <cen4ukk@gmail.com>
2017-02-26 17:20:20 +0200

    Initial commit

---
+++ src/lab1/Allocator.java
@@ -0,0 +1,8 @@
+package lab1;
+
+/**
+ * Created by allugard on 26.02.17.
+ */
+public class Allocator {
+
+}
---
+++ src/lab1/Main.java
@@ -0,0 +1,10 @@
+package lab1;
+
+/**
+ * Created by allugard on 26.02.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
