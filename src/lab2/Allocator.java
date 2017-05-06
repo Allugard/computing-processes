6149893efa4313a52f1a3f85793b8bfd818ae409
Allugard <cen4ukk@gmail.com>
2017-05-06 16:41:26 +0300

    finished alloc()

--- src/lab2/Allocator.java
+++ src/lab2/Allocator.java
@@ -1,3 +1,4 @@
+
 package lab2;
 
 import java.util.*;
@@ -7,18 +8,23 @@ import java.util.*;
  */
 public class Allocator {
     private int [] memory;
-    private int [] pages;
+    private ArrayList <Integer> pages = new ArrayList<>();
     private LinkedList<Integer> freePages = new LinkedList<>();
     private Map<Integer, Integer> blockPages = new HashMap<>();
+    /**
+     * first pos - free(0)/page in block(1)/page with blocks(2)
+     * second - next page(1)/number of free blosk (2)
+     * third - position of next free block(2)
+     */
     private static final int HEADER = 3;
     private final int pageSize;
 
     public Allocator(int mem, int pages) {
-        this.pages = new int[pages];
+//        this.pages = new int[pages];
         this.memory = new int[(mem+HEADER)*pages];
-        for (int i = 1; i <pages; i++) {
-            this.pages[i] = i*(mem+HEADER);
-            freePages.add(this.pages[i]);
+        for (int i = 0; i <pages; i++) {
+            this.pages.add(i*(mem+HEADER));
+            freePages.add(this.pages.get(i));
         }
         pageSize = mem;
     }
@@ -30,7 +36,6 @@ public class Allocator {
                 memory[position+1]--;
                 int buf = memory[position + memory[position+2]];
                 int pos = position + memory[position+2];
-                memory[pos] = 1;
                 memory[position+2] = buf;
                 return pos;
             }else {
@@ -38,15 +43,45 @@ public class Allocator {
                 freePages.removeFirst();
                 blockPages.put(size, position);
                 int numBlocks = pageSize/size;
-                for (int i = 0; i < numBlocks; i++) {
-
+                memory[position] = numBlocks;
+                memory[position+1] = numBlocks - 1;
+                memory[position+2] = HEADER + size;
+                for (int i = 1; i < numBlocks - 1; i++) {
+                    memory[position+HEADER + i*size] = HEADER + (i+1)*size;
                 }
 
-                return position;
+                return position+HEADER;
             }
         }else {
-            return 0;
+            int numPages = size/pageSize + 1;
+            int position = freePages.getFirst();
+            freePages.removeFirst();
+            for (int i = 0; i < numPages; i++) {
+                int nextPos = freePages.getFirst();
+                freePages.removeFirst();
+                memory[position] = 1;
+                if(i != numPages - 1) {
+                    memory[position + 1] = pages.indexOf(nextPos);
+                    position = nextPos;
+                }
+
+            }
+            return position;
         }
     }
 
+    public void free(){
+        pages.contains(5);
+    }
+
+    public void dump(){
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Allocator{" +
+                "memory=" + Arrays.toString(memory) +
+                '}';
+    }
 }
--- src/lab2/Main.java
+++ src/lab2/Main.java
@@ -4,4 +4,10 @@ package lab2;
  * Created by allugard on 05.05.17.
  */
 public class Main {
+    public static void main(String[] args) {
+        Allocator allocator = new Allocator(20, 5);
+        allocator.alloc(2);
+        allocator.alloc(5);
+        allocator.dump();
+    }
 }
