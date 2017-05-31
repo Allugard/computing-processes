ccc54467ee4325e0b923258b4c794a6efffe3a1a
Allugard <cen4ukk@gmail.com>
2017-05-31 19:51:43 +0300

    removed duplicate

--- src/lab3/Statistics.java
+++
@@ -1,7 +0,0 @@
-package lab3;
-
-/**
- * Created by allugard on 18.05.17.
- */
-public class Statistics {
-}
---
+++ src/main/java/lab3/PlotPainter.java
@@ -0,0 +1,7 @@
+package lab3;
+
+/**
+ * Created by allugard on 19.05.17.
+ */
+public class PlotPainter {
+}
